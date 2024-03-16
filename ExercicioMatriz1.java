5fdaae9a79ce511c5f64cc344ee2500a206fb96f
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2024-03-16 09:21:02 -0300

    Update and rename quesito1.java to ExercicioMatriz1.java

--- ExercicioMatriz1.java
+++ ExercicioMatriz1.java
@@ -1,5 +1,5 @@
 
-public class quesito1 {
+public class ExercicioMatriz1 {
     public static void main(String[] args){
         int matriz[][] = {{2,3,4,5},{6,7,8,9}};
         for (int i = 0; i < matriz.length; ++i){
