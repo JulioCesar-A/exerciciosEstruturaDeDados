e96dfa0fe23939aaf4af486805ff923044a8d1d3
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2024-03-16 09:21:55 -0300

    Update and rename quesito2.java to ExercicioMatriz2.java

--- ExercicioMatriz2.java
+++ ExercicioMatriz2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class quesito2 {
+public class ExercicioMatriz2 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int somatotal = 0;
